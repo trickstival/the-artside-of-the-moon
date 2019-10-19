c2a3f9046edd7abf3537983d922d06f26ec9d2a8
trickstival <trick_stival@hotmail.com>
2019-10-19 00:21:04 -0300

    added moon animation

--- src/components/Moon3D.vue
+++ src/components/Moon3D.vue
@@ -1,32 +1,69 @@
 <template>
   <div class="moon-3d">
     <a-scene embedded>
+        <a-camera :position="cameraPos"></a-camera>
         <a-sky color="black"></a-sky>
-        <a-light type="point" color="white" intensity="0.1" position="-100 0 0"></a-light>
         <a-sphere radius="4" ref="moon" :src="$options.moonImg" position="0 1.5 -7">
         </a-sphere>
         <a-light type="point" color="white"></a-light>
 
         <a-sphere roughness="-5" ref="sun" color="#ffe484" position="-130 1.5 -130">
         </a-sphere>
+
+        <a-sphere radius="40" roughness="0" ref="earth" color="blue" position="0 1.5 800">
+        </a-sphere>
     </a-scene>
   </div>
 </template>
 
 <script>
+import TWEEN from '@tweenjs/tween.js'
 import moonImg from '../assets/crop.jpg'
 export default {
     moonImg,
-    mounted () {
-        const el = this.$refs.moon
-        const rotate = () => {
-            el.object3D.rotation.y += .002;
-            requestAnimationFrame(rotate)
+    data () {
+      return {
+        camera: {
+          x: 0,
+          y: 1.5,
+          z: 500
         }
-        rotate()
+      }
+    },
+    computed: {
+      cameraPos () {
+        const { x, y, z } = this.camera
+        return `${x} ${y} ${z}`
+      }
+    },
+    methods: {
+      rotateMoon () {
+        const el = this.$refs.moon
+        el.object3D.rotation.y += .002;
+      },
+      shipCamera () {
+        if (this.camera.z === 0) {
+          return
+        }
+        this.camera.z -= 2
+      }
+    },
+    mounted () {
+        const animate = (time) => {
+            this.rotateMoon()
+            // this.shipCamera()
+            TWEEN.update(time)
+            requestAnimationFrame(animate)
+        }
+        requestAnimationFrame(animate)
 
-        // With .setAttribute (less recommended).
-        el.setAttribute('rotation', {x: 15, y: 30, z: 0});
+        const cameraTween = new TWEEN.Tween(this.camera)
+          .to({ z: 0 }, 20000)
+          .easing(TWEEN.Easing.Quadratic.Out)
+          .start()
+
+        this.$refs.moon.setAttribute('rotation', {x: 15, y: 30, z: 0});
+        this.$refs.earth.object3D.shininess = 0
     }
 };
 </script>
