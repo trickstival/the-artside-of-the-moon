5607500396e367ccdbd2acc0c8afcd7bea32d894
trickstival <trick_stival@hotmail.com>
2019-10-19 23:14:13 -0300

    add right speechs

--- src/components/Speech.vue
+++ src/components/Speech.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="speech">
         <transition name="fade" mode="out-in">
-            <h1 v-if="currentSpeech" class="current-speech">
-                {{ currentSpeech && currentSpeech.text }}
+            <h1 v-if="currentSpeech.text" class="current-speech">
+                <q>
+                    {{ currentSpeech && currentSpeech.text }}
+                </q>
+                <br>
+                <i class="author">
+                    — {{ currentSpeech.author }}
+                </i>
             </h1>
         </transition>
     </div>
@@ -18,29 +24,34 @@ export default {
             currentSpeech: '',
             speechs: [
                 {
-                    text: 'Isso aí meu povo',
-                    duration: 2000
+                    text: '',
+                    duration: 18000
                 },
                 {
-                    text: 'Teste teste',
-                    duration: 2000
+                    text: 'Al, It‘s been a long way, but we‘re here.',
+                    duration: 5000,
+                    author: 'Alan Shepard'
                 },
                 {
-                    text: 'Eu',
-                    duration: 2000
+                    text: 'That’s one small step for a man, one giant leap for mankind.',
+                    duration: 5000,
+                    author: 'Neil Armstrong'
                 },
                 {
-                    text: 'sou',
-                    duration: 2000
+                    text: 'In presence of the moon nobody sees stars.',
+                    duration: 5000,
+                    author: 'James Lowell'
                 },
                 {
-                    text: 'Muito',
-                    duration: 2000
+                    text: 'I think it is very beautiful that 50 years after Apollo, the Artemis program will carry the next man and the first woman to the Moon  ',
+                    duration: 6000,
+                    author: 'Jim Bridenstine'
                 },
                 {
-                    text: 'Rápido',
-                    duration: 2000
-                }
+                    text: 'I have a daughter who is 11 years old, and I want her to be able to see herself in the same role as the next women that go to the Moon.',
+                    duration: 6000,
+                    author: 'Jim Bridenstine'
+                },
             ],
             cancelRunning: false
         }
@@ -82,6 +93,7 @@ export default {
     height: 100%;
 }
 .current-speech {
+    padding-right: 80px;
     grid-column: 2;
     text-align: center;
 }
